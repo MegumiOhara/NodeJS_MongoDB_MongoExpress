f331c5b75894a6c744db56e32b7e3cc664d6495d
MegumiOhara <156937802+MegumiOhara@users.noreply.github.com>
2024-12-02 14:49:12 +0100

    GET and POST

--- index.js
+++ index.js
@@ -6,10 +6,42 @@ app.use(express.json());
 
 mongoose.connect("mongodb://localhost:27017/firstZeldadatabase")
     .then(() => console.log("Connected to MongoDB"))
-    .catch((err) => console.log("Could not connect to MongoDB", error));
+    .catch((err) => console.log("Could not connect to MongoDB", err));
 
+ //Define a schema and a model.    
 const itemSchema = new mongoose.Schema({
     name: String,
-    releaseDate: Number, 
-    console: Number,
+    release: Number, 
+    console: String,
 });
+
+const Item = mongoose.model("Item", itemSchema);
+
+//Fetch data 
+app.get("/items", async(req,res) => {
+    try {
+        const newItem = new Item(req.body);
+        const items = await Item.find();
+        res.json(items);
+    } catch (error) {
+        res.status(500).json({
+            error: "Failed to get items",
+        });
+    }
+});
+
+//Add data 
+app.post("/items", async(req,res)=>{
+    try {
+        const newItem = new Item(req.body);
+        await newItem.save();
+        res.json(newItem);
+    } catch (error) {
+        res.status(500).json({
+            error: "Failed to post items",
+        });
+    }
+});
+
+//start server 
+app.listen(3000,() => console.log("Server is running at http://localhost:3000"));
